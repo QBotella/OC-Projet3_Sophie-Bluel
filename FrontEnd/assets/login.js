6e11a59dfdbdff7c44e5ebd0d70b987625485d89
Quentin Botella <quentin_botella@yahoo.fr>
2024-05-14 18:47:25 +0200

    Create login function

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,46 @@
+async function login(email, password) {
+    try {
+        // Create a POST request to your login endpoint
+        const response = await fetch('http://localhost:5678/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: email, password: password })
+        });
+
+        // Check if the request was successful
+        if (response.ok) {
+            const data = await response.json();
+            // Process the response display a success message
+            console.log('Connexion réussie !', data);
+            // Redirect to another page
+            window.location.href = "index.html";
+        } else {
+            // If the request fails, display an error message
+            console.error('Erreur de connexion', response.status);
+             // Display an error message to the user
+            alert('Erreur dans l’identifiant ou le mot de passe');
+        }
+    } catch (error) {
+        console.error('Erreur de connexion', error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById('loginForm');
+
+    // Function to handle form submission
+    function handleFormSubmit(event) {
+        event.preventDefault(); // Prevent the form from submitting normally
+
+        // Get the values of the email and password fields
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        // Call the login function
+        login(email, password);
+    }
+
+    loginForm.addEventListener('submit', handleFormSubmit);
+});
